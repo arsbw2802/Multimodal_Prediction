bd29908e30d24e5ce360e33625b55e3762845c86
Kevin Daniel <kdaniel48@gatech.edu>
2025-04-03 15:29:21 -0400

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# main file
+# TODO
+# parse input arguments
+# perform data preprocessing for both imu and tdost
+# split data into test, train, val
+# train data on both imu and tdost
+# get probabilities from both classifiers
+# average probabilities and make final prediction
